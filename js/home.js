2f20bd8b738816e67460d604075ab7704635eba7
Coded-Adil <itsadil2022@gmail.com>
2024-09-20 01:59:15 +0600

    added add money input section and get the value of the inputs

--- js/home.js
+++ js/home.js
@@ -1,1 +1,9 @@
-console.log('Home added');
+// console.log('Home added');
+
+document.getElementById('button-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+    const addMoney = document.getElementById('add-money-input').value;
+    const pinNumber = document.getElementById('pin-number-input').value;
+    console.log(addMoney);
+    console.log(pinNumber);
+})
