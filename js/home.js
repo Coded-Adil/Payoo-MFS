aca9bf3ce8afb6b3f8eca0cfb6c302f2f10955d4
Coded-Adil <itsadil2022@gmail.com>
2024-09-20 03:59:54 +0600

    add money is adding ammount if the pin is correct

--- js/home.js
+++ js/home.js
@@ -6,4 +6,19 @@ document.getElementById('button-add-money').addEventListener('click', function(e
     const pinNumber = document.getElementById('pin-number-input').value;
     console.log(addMoney);
     console.log(pinNumber);
+
+    if (pinNumber === '1234') {
+        // Get the Current Balance
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(balance);
+        const addMoneyNumber = parseFloat(addMoney);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber+balanceNumber;
+        console.log(newBalance);
+
+        // Update balance
+        document.getElementById('account-balance').innerText = newBalance;
+    }else{
+        alert("Failed to add money");
+    }
 })
